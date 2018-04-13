d9b6d9139cebf100161ca494997dce8f65b1eec3
Awesome Coder <pinku8kumar@yahoo.com>
2018-04-13 17:52:13 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const fs = require('fs')
+const hbs = require('hbs');
+
+const app = express();
+
+//setting the view template engine
+app.set ('view engine','hbs');
+//to use partials from a specific directory
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+
+//middleware
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var logMessage = `${now} : ${req.method} : ${req.url} \r\n`
+  fs.appendFile('server.log', logMessage, (err) => {
+    if (err){
+      console.log('Error!');
+    }
+  })
+  console.log(`${logMessage} :`);
+  next();
+
+})
+
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+});
+
+//__dirname gives the root of the project folder
+app.use(express.static(__dirname + '/public'))
+
+//to use helper functions
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+
+})
+
+hbs.registerHelper('upperCase', (text) =>{
+  return text.toUpperCase();
+
+})
+
+app.get('/',(req, res) => {
+  res.render('home.hbs',{
+    pageName :'Home Page',
+  //  currentYear : new Date().getFullYear(), //changed to a Helper
+    welcomeMessage: 'Welcome to my first website using NodeJs and Handlebars!'
+  })
+
+  //Comment - sending string with html
+//  res.send('<h1> Hello Express </h1>');
+})
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageName :'About Us'
+    //currentYear : new Date().getFullYear() - changed to a Helper
+  });
+  // res.send({
+  //   name: 'Web Server Project',
+  //   version: '1.0.0'
+  // });
+})
+
+app.get('/bad',(req,res) =>{
+  res.send({
+    errorMessage:'Oops something went wrong'
+  })
+})
+
+app.listen(3000,()=>{
+  console.log('Server is up on port 3000')
+});
